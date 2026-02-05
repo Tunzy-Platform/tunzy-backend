1215040343cf53f7e3765a7bc2b7b9f9d57785d1
ali sadeghi far <sadeghifarali9@gmail.com>
2026-02-05 14:28:58 +0330

    implement downloading

--- app/soundcloud/download.py
+++ app/soundcloud/download.py
@@ -1,1 +1,39 @@
-async def download_hls(tack_id: str) -> dict: ...
+from app.core.logging import setup_logging
+from app.core import config
+import asyncio
+import yt_dlp
+
+logger = setup_logging(__name__)
+
+
+def sync_download_ytdl(links: list[str]):
+    with yt_dlp.YoutubeDL(config.ydl_opts) as ydl:
+        ydl.download(links)
+
+
+async def download_tracks(
+    track_urls: list[str],
+) -> None:
+
+    track_urls = list(set(track_urls))
+
+    download_buckets: list[list[str]] = []
+    bucket_size: int = (
+        len(track_urls) + config.settings.concurrent_downloads
+    ) // config.settings.concurrent_downloads
+    for i in range(0, len(track_urls), bucket_size):
+        bucket = track_urls[i : i + bucket_size]
+        download_buckets.append(bucket)
+
+    logger.info(
+        "Start downloading %s",
+        track_urls,
+    )
+    tasks = []
+    for i, _bucket in enumerate(download_buckets):
+        logger.debug("Bucket %d data: %s", i, _bucket)
+        task = asyncio.create_task(asyncio.to_thread(sync_download_ytdl, _bucket))
+
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+    logger.info("Finish downloading %s", track_urls)
