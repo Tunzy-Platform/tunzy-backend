fea2bb0b917cf66c6ef75dfae2ea7a98868ee0f2
ali sadeghi far <sadeghifarali9@gmail.com>
2026-02-05 14:29:21 +0330

    main pipline

--- app/soundcloud/main.py
+++ app/soundcloud/main.py
@@ -1,21 +1,15 @@
-# /// script
-# requires-python = ">=3.12"
-# dependencies = [
-#     "run",
-# ]
-# ///
 from app.core import config
 from app.core.logging import setup_logging
 from app.soundcloud.auth import SoundCloudAuth, get_app_version, get_client_id, headers
-from app.soundcloud.playlist import get_playlists
+from app.soundcloud.playlist import get_playlists, get_liked_tracks, get_playlist_tracks
 import asyncio
 import aiohttp
-
+from app.soundcloud.download import download_tracks
 logger = setup_logging(__name__)
 
 
 async def main():
-    logger.info("headers: %s", headers)
+    # logger.info("headers: %s", headers)
     async with aiohttp.ClientSession(
         proxy=config.settings.http_proxy,
         headers=headers,
@@ -24,8 +18,11 @@ async def main():
         app_version = await get_app_version(session)
         sc_auth = SoundCloudAuth(client_id, app_version)
         res = await get_playlists(session, sc_auth)
-        with open("res.json", "w") as file:
-            file.write(str(res))
+        logger.info("selected playlist: %s", res[0])
+        res = await get_playlist_tracks(res[0]["permalink_url"], session, sc_auth)
 
+        permalink_urls = [track["permalink_url"] for track in res]
+        # logger.info("Data: %s", res)
+        await download_tracks(permalink_urls)
 
 asyncio.run(main())
