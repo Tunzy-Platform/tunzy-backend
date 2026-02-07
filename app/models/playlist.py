c4e951159addd40db7075068243a9a94a7ee409d
ali sadeghi far <sadeghifarali9@gmail.com>
2026-02-08 02:28:05 +0330

    palylist and tracks models

---
+++ app/models/playlist.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from sqlmodel import Field, SQLModel, Relationship, UniqueConstraint
+
+from app.schemas.playlist import PlaylistSchema, TrackSchema
+
+
+class PlaylistTrackLinkModel(SQLModel, table=True):
+    playlist_id: int | None = Field(
+        default=None,
+        foreign_key="playlistmodel.id",
+        primary_key=True,
+    )
+    track_id: int | None = Field(
+        default=None,
+        foreign_key="trackmodel.id",
+        primary_key=True,
+    )
+
+
+class PlaylistBaseModel(SQLModel):
+    platform_id: str | None = Field(index=True)
+    url: str | None = Field()
+    name: str = Field()
+    owner: str | None = Field()
+    track_count: int = Field()
+    duration: int = Field()
+    thumbnail: str | None = Field()
+    is_synced: bool = Field()
+    last_modified: datetime = Field()
+    service: str = Field()
+
+    @classmethod
+    def from_schema(cls, schema: PlaylistSchema) -> "PlaylistBaseModel":
+        return cls(**schema.model_dump())
+
+    def update_from_schema(self, schema: PlaylistSchema):
+        self.sqlmodel_update(schema.model_dump())
+
+
+class PlaylistModel(PlaylistBaseModel, table=True):
+    __table_args__ = (
+        UniqueConstraint("platform_id", "service", name="platform_id_service_unique"),
+    )
+    id: int | None = Field(primary_key=True)
+    tracks: list["TrackModel"] = Relationship(
+        back_populates="playlists", link_model=PlaylistTrackLinkModel
+    )
+
+
+class PlaylistPublicModel(PlaylistBaseModel):
+    id: int | None
+
+
+class PlaylistCreateModel(PlaylistBaseModel): ...
+
+
+class TrackBaseModel(SQLModel):
+    platform_id: str = Field(index=True)
+    url: str | None = Field()
+    name: str = Field()
+    artist_name: str | None = Field()
+    album: str | None = Field()
+    duration: int = Field()
+    is_synced: bool = Field()
+    thumbnail: str | None = Field()
+
+    @classmethod
+    def from_schema(cls, schema: TrackSchema) -> "TrackBaseModel":
+        return cls(**schema.model_dump())
+
+    def update_from_schema(self, schema: TrackSchema) -> None:
+        self.sqlmodel_update(
+            schema.model_dump(),
+        )
+
+
+class TrackModel(TrackBaseModel, table=True):
+    id: int | None = Field(primary_key=True, index=True)
+    file_path: str | None = Field()
+
+    playlists: list["PlaylistModel"] = Relationship(
+        back_populates="tracks", link_model=PlaylistTrackLinkModel
+    )
+
+
+class TrackPublicModel(TrackBaseModel):
+    id: int
+    file_path: str | None
